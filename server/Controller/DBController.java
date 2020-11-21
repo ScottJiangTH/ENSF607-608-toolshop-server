801635bc9a930405ad5971d802d38291e9d7ed23
Karenzhang7717 <64809520+Karenzhang7717@users.noreply.github.com>
2020-11-21 16:03:26 -0700

    updated DBController

--- server/Controller/DBController.java
+++ server/Controller/DBController.java
@@ -1,82 +1,109 @@
 package server.Controller;
-import java.io.File;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+import server.Model.Item;
+import server.Model.Supplier;
 
 
-/**
- * FileManager is in charge of reading files.
- * @author Yunying Zhang
- * @since October 10 2020
- *
- */
+
 public class DBController {
-	private ArrayList<Supplier> supList = new ArrayList<Supplier>();
-	private SupplierList supplierList = new SupplierList(supList);
-	private ArrayList<Item> toolList = new ArrayList<Item>();
-	
-	/**
-	 * Reads and parses two files
-	 * @param file1: Item file 
-	 * @param file2: Supplier file
-	 * @throws Exception
-	 */
-	public DBController(String file1, String file2)throws Exception {
-		
-		
-		File itemFile = new File(file1);
-		File supplierFile = new File(file2);
-		Scanner sc = new Scanner(itemFile);
-		Scanner sc1 = new Scanner(supplierFile);
+	private ArrayList<Item> items;
+	private ArrayList<Supplier> suppliers;
+//
+//	private String conString = "jdbc:mysql://localhost:3306/company";
+//	private String username = "root";
+//	private String password = "xxxxx";
 
-		//creating tool list
-	    while (sc.hasNextLine()) { 
-	    	String item = sc.nextLine();
-	    	String[] toolInfo = item.split(";");
-	    	
-	    	int toolID = Integer.parseInt(toolInfo[0]);
-	    	String toolName = toolInfo[1];
-	    	int toolQty = Integer.parseInt(toolInfo[2]);
-	    	double toolPrice = Double.parseDouble(toolInfo[3]);
-	    	int supplierID = Integer.parseInt(toolInfo[4]);
-	    	Item tool = new Item(toolID,toolName,toolQty,toolPrice,supplierID);
-	    	
-	    	toolList.add(tool);
-	    	
-	    }
-	    
-	    //creating supplier list
-	    while (sc1.hasNextLine()) { 
-	    	
-	    	String sup = sc1.nextLine();
-	    	String[] supInfo = sup.split(";");
-	    	
-	    	int supID = Integer.parseInt(supInfo[0]);
-	    	String supName = supInfo[1];
-	    	String supAddress = supInfo[2];
-	    	String supRep = supInfo[3]; 
-	    	Supplier supplier = new Supplier(supID,supName,supAddress,supRep);
-	    	
-	    	supplierList.addSupplier(supplier);
-	    }
-	    
-	    sc.close();
-	    sc1.close();
-	    
-	    
-	   
+
+	private Supplier findSupplier(int supplierId) {
+		Supplier theSupplier = null;
+		for (Supplier s : suppliers) {
+			if (s.getSupId() == supplierId) {
+				theSupplier = s;
+				break;
+			}
+		}
+		return theSupplier;
 	}
-	
 
-	public SupplierList generateSupList(){
-		return supplierList;
-	} 
-	
-	public ArrayList<Item> generateToolList(){
-		return toolList;
-	} 
-	
-	
-	
+	public ArrayList<Item> readItemsFromDatabase()
+	{
+		this.items = new ArrayList<Item>();
+
+
+
+		String sql = "SELECT * FROM items";
+		try
+		{
+			String conString = "jdbc:mysql://localhost:3306/toolshop";
+			String username = "root";
+			String password = "xxxxx";
+			Connection con = DriverManager.getConnection(conString,  username,  password);
+			Statement s = con.prepareStatement(sql);
+			ResultSet rs = s.executeQuery(sql);
+
+			while(rs.next())
+			{
+				String ID = rs.getString(1);
+				String name = rs.getString(2);
+				String quantity = rs.getString(3);
+				String price = rs.getString(4);
+				String supplierID = rs.getString(5);
+				Supplier theSupplier = findSupplier(Integer.parseInt(supplierID));
+
+				Item myItem = new Item(Integer.parseInt(ID), name, Integer.parseInt(quantity), Double.parseDouble(price), theSupplier);
+				items.add(myItem);
+				theSupplier.getItemList().add(myItem);
+			}
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+
+		return items;
+	}
+
+
+	public ArrayList<Supplier> readSuppliersFromDatabase()
+	{
+		this.suppliers = new ArrayList<Supplier>();
+
+		String sql = "SELECT * FROM suppliers";
+		try
+		{
+			String conString = "jdbc:mysql://localhost:3306/toolshop";
+			String username = "root";
+			String password = "xxxxx";
+			Connection con = DriverManager.getConnection(conString,  username,  password);
+			Statement s = con.prepareStatement(sql);
+			ResultSet rs = s.executeQuery(sql);
+
+			while(rs.next())
+			{
+				String supplierID = rs.getString(1);
+				String companyName = rs.getString(2);
+				String address = rs.getString(3);
+				String salesContact = rs.getString(4);
+				suppliers.add(new Supplier(Integer.parseInt(supplierID), companyName, address, salesContact));
+			}
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+
+		return suppliers;
+	}
 
 }
+	
+
+
