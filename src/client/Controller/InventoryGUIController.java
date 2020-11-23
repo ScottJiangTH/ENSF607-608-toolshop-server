3c3eeeb55e681e1e7205fb29d1c7859826ae1e38
Karenzhang7717 <64809520+Karenzhang7717@users.noreply.github.com>
2020-11-22 22:18:05 -0700

    updated GUI for toolshop

--- src/client/Controller/InventoryGUIController.java
+++ src/client/Controller/InventoryGUIController.java
@@ -1,5 +1,99 @@
 package client.Controller;
 
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import client.View.InventoryManagementGUI;
+
 public class InventoryGUIController {
 
+	InventoryManagementGUI view;
+
+	ClientController client;
+
+	public InventoryGUIController(InventoryManagementGUI view, ClientController client) {
+		this.view = view;
+		this.client = client;
+
+		this.view.addBrowseListener(new BrowseListener());
+		this.view.addIDListener(new IDListener());
+		this.view.addNameListener(new NameListener());
+		this.view.addCheckItemListener(new CheckItemListener());
+		this.view.addDecreaseItemListener(new DecreaseItemListener());
+		this.view.addPrintOrderListener(new PrintOrderListener());
+	}
+
+	class CheckItemListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+	}
+
+	class IDListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+
+	}
+
+	class BrowseListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+	}
+
+	class NameListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+
+	class DecreaseItemListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+
+	}
+
+	class PrintOrderListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+
+	}
+	class AddToolListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+
+	}
+	class DeleteToolListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+		InventoryManagementGUI g = new InventoryManagementGUI(Toolkit.getDefaultToolkit().getScreenSize());
+		// ClientController client = new ClientController("localhost", 9009);
+		// InventoryGUIController c = new InventoryGUIController(g, client);
+		g.pack();
+		g.setVisible(true);
+	}
 }
--- src/client/View/InventoryManagementGUI.java
+++ src/client/View/InventoryManagementGUI.java
@@ -1,5 +1,99 @@
 package client.View;
 
-public class InventoryManagementGUI {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class InventoryManagementGUI extends JFrame {
+
+	private JButton browseTool = new JButton("List Tools");
+	private JButton searchName = new JButton("Search by Name");
+	private JButton searchID = new JButton("Search by ID");
+	private JButton checkQuantity = new JButton("Check Quantity");
+	private JButton decreaseQuantity = new JButton("Decrease Quantity");
+	private JButton printOrder = new JButton("Print Order");
+	private JButton addTool = new JButton("Add Tool");
+	private JButton deleteTool = new JButton("Delete Tool");
+	private JLabel listLabel = new JLabel("List of Tools");
+	private JTable listTable;
+	private JScrollPane listScroll;
+
+	public InventoryManagementGUI(Dimension dim) {
+		setTitle("Tool Shop Application");
+		setBounds(0, 0, dim.width/2, dim.height/2);
+		setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		
+		JPanel labels = new JPanel();
+		labels.add(listLabel);
+		
+		JPanel txtArea = new JPanel();
+		String[] colName = {"Item ID", "Item Name", "Item Quantity"};
+		
+		DefaultTableModel tableModel = new DefaultTableModel(colName, 10);
+		listTable = new JTable(tableModel);
+		listScroll = new JScrollPane(listTable);
+
+		listTable.setEnabled(false);
+		listTable.setFillsViewportHeight(true);
+		txtArea.add(listScroll);
+		
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new GridLayout(2,3));
+		buttons.add(browseTool);
+		buttons.add(searchName);
+		buttons.add(searchID);
+		buttons.add(checkQuantity);
+		buttons.add(decreaseQuantity);
+		buttons.add(checkQuantity);
+		buttons.add(printOrder);
+		buttons.add(addTool);
+		buttons.add(deleteTool);
+		
+		getContentPane().add(labels, BorderLayout.NORTH);
+		getContentPane().add(new JScrollPane(txtArea), BorderLayout.CENTER);
+		getContentPane().add(buttons, BorderLayout.SOUTH);
+	}
+
+	public void addBrowseListener(ActionListener browseListener) {
+		browseTool.addActionListener(browseListener);
+	}
+
+	public void addIDListener(ActionListener idListener) {
+		searchID.addActionListener(idListener);
+	}
+
+	
+	public void addNameListener(ActionListener nameListener) {
+		searchName.addActionListener(nameListener);
+	}
+
+	
+	public void addCheckItemListener(ActionListener checkItemListener) {
+		checkQuantity.addActionListener(checkItemListener);
+	}
+
+	
+	public void addDecreaseItemListener(ActionListener decreaseItemListener) {
+		decreaseQuantity.addActionListener(decreaseItemListener);
+	}
+
+	public void addPrintOrderListener(ActionListener printOrderListener) {
+		printOrder.addActionListener(printOrderListener);
+	}
+	public void addAddToolListener(ActionListener addTool) {
+		printOrder.addActionListener(addTool);
+	}
+	public void deleteToolListener(ActionListener deleteTool) {
+		printOrder.addActionListener(deleteTool);
+	}
+
+	public void setTableModel(DefaultTableModel m) {
+		listTable.setModel(m);
+	}
 
 }
