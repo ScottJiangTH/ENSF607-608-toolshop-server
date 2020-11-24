28163b054dc95fcfac4323cab0029f4c0cbe7596
Scott Tianhan Jiang <64949105+ScottJiangTH@users.noreply.github.com>
2020-11-24 13:26:10 -0700

    Update ClientController.java

--- src/client/Controller/ClientController.java
+++ src/client/Controller/ClientController.java
@@ -1,10 +1,23 @@
 package client.Controller;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
+import org.json.*;
+
+/**
+ * This class serves as a socket manager and receive/send communication Strings
+ * from/to ServerController. It uses ClientModel to process some JSON formatted
+ * String into objects, and pass objects into each view controller.
+ *
+ */
 public class ClientController {
 
 	Socket socket;
@@ -26,47 +39,29 @@ public class ClientController {
 	}
 
 	public DefaultTableModel getItemList() {
-		String input;
-		String[] item;
 
-<<<<<<< Updated upstream
-		DefaultTableModel m = new DefaultTableModel();
-		m.setColumnIdentifiers(new String[] { "Item ID", "Item Type", "Item Name","Item Description","Item Price","Item Quantity","Supplier Id","Already Ordered" });
-		socketOut.println("option,1");
-=======
 		String command = "option,1";
 		socketOut.println(command);
 		
 		DefaultTableModel m = new DefaultTableModel();
 		m.setColumnIdentifiers(new String[] { "Item ID", "Item Name", "Item Quantity" });
 		
->>>>>>> Stashed changes
 		try {
-			while ((input = socketIn.readLine())!= null) {
-				item = input.split(",");
-			//	item = input;
-				m.addRow(item);
+			String json = socketIn.readLine();
+			JSONArray itemList = new JSONArray(json);
+			for (int i = 0; i < itemList.length();i++) {
+				String itemId = itemList.getJSONObject(i).getString("itemId");
+				String itemName = itemList.getJSONObject(i).getString("itemName");
+				String itemQuantity = itemList.getJSONObject(i).getString("itemQuantity");
+				String s[] = {itemId, itemName, itemQuantity};
+				m.addRow(s);
 			}
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "The server disconnected");
 		}
 		return m;
 	}
-	
-//	public void getItemList() {
-//		socketOut.println("option,1");
-//	}
 
-<<<<<<< Updated upstream
-	public String getItemByID(int ID) {
-		String input = "", item = "";
-		socketOut.println("option,2");
-		socketOut.println(ID);
-		try {
-			while ((input = socketIn.readLine())!= null) {
-				item += input;
-			}
-=======
 	public String getItemByID(int itemId) {
 		String command = "option,2," + itemId;
 		socketOut.println(command);
@@ -75,7 +70,6 @@ public class ClientController {
 			String json = socketIn.readLine();
 			JSONObject item = new JSONObject(json);
 			String s = "";
->>>>>>> Stashed changes
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "The server disconnected");
 		}
@@ -85,12 +79,12 @@ public class ClientController {
 
 	public String getItemByName(String name) {
 		String input = "";
-		socketOut.println("option,3");
+		socketOut.println("3");
 		socketOut.println(name);
 		String item = "";
 
 		try {
-			while ((input = socketIn.readLine())!= null) {
+			while ((input = socketIn.readLine()) != null) {
 				item += input;
 			}
 		} catch (IOException e) {
@@ -99,27 +93,14 @@ public class ClientController {
 
 		return item;
 	}
+
 //need to add: search quantity by id
-	public String getQuantityById(int ID) {
+	public String getQuantity(String name) {
 		String input = "", quantity = "";
-		socketOut.println("option,4");
-		socketOut.println(ID);
-		try {
-			while ((input = socketIn.readLine())!= null) {
-				quantity += input;
-			}
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "The server disconnected");
-		}
-		return quantity;
-	}
-	
-	public String getQuantityByName(String name) {
-		String input = "", quantity = "";
-		socketOut.println("option,5");
+		socketOut.println("5");
 		socketOut.println(name);
 		try {
-			while ((input = socketIn.readLine())!= null) {
+			while ((input = socketIn.readLine()) != null) {
 				quantity += input;
 			}
 		} catch (IOException e) {
@@ -130,11 +111,11 @@ public class ClientController {
 
 	public String decreaseItemQuantity(String name) {
 		String input = "", message = "";
-		socketOut.println("option,6");
+		socketOut.println("6");
 		socketOut.println(name);
 
 		try {
-			while ((input = socketIn.readLine())!= null) {
+			while ((input = socketIn.readLine()) != null) {
 				message += input;
 			}
 		} catch (IOException e) {
@@ -147,7 +128,7 @@ public class ClientController {
 	public String getPrintOrder() {
 		String input = "", order = "";
 		socketOut.println("1");
-		//need to read from json file passed
+		// need to read from json file passed
 //		try {
 //			while ((input = socketIn.readLine()).charAt(0) != '\0') {
 //				order += input + "\n";
@@ -160,21 +141,22 @@ public class ClientController {
 
 	public void removeTool(String name) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void addTool(String name) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	//for customer management - need to think more
-	
+
+	// for customer management - need to think more
+
 	private int id;
-	private String firstName,lastName,address,postalCode,phoneNumber;
+	private String firstName, lastName, address, postalCode, phoneNumber;
 	private char type;
-	
-	public ClientController(int id,String firstName,String lastName,String address,String postalCode,String phoneNumber,char type) {
+
+	public ClientController(int id, String firstName, String lastName, String address, String postalCode,
+			String phoneNumber, char type) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -183,73 +165,74 @@ public class ClientController {
 		this.phoneNumber = phoneNumber;
 		this.type = type;
 	}
-	
+
 	public int getID() {
 		return id;
 	}
+
 	public void setID(int id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getPostalCode() {
 		return postalCode;
 	}
+
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public char getType() {
 		return type;
 	}
+
 	public void setType(char type) {
 		this.type = type;
 	}
-	
+
 	public boolean compare(ClientController temp) {
-		if (this.id==temp.id && this.firstName==temp.firstName && this.lastName==temp.lastName && this.address==temp.address && this.postalCode==temp.postalCode && this.type==temp.type) {
+		if (this.id == temp.id && this.firstName == temp.firstName && this.lastName == temp.lastName
+				&& this.address == temp.address && this.postalCode == temp.postalCode && this.type == temp.type) {
 			return true;
-		}
-		else return false;
-	}
-	public String toString() {
-		return (this.getID()+" ; "+this.getFirstName()+" ; "+this.getLastName()+" ; "+this.getAddress()+" ; "+this.getPostalCode()+" ; "+this.getPhoneNumber()+" ; "+this.getType());
+		} else
+			return false;
 	}
 
-	public void addSupplier(String name) {
-		// TODO Auto-generated method stub
-		//need to add supplier to DB
-		
-		socketOut.println("option,6");
-		
+	public String toString() {
+		return (this.getID() + " ; " + this.getFirstName() + " ; " + this.getLastName() + " ; " + this.getAddress()
+				+ " ; " + this.getPostalCode() + " ; " + this.getPhoneNumber() + " ; " + this.getType());
 	}
-	
-	public void removeSupplier(String name) {
-		// TODO Auto-generated method stub
-		//need to add supplier to DB
-		
-	}
-	
-	
+
 }
